fede34a48ea680d5fb4a1484a0bec3d3af2f17d7
Arpityadav11 <arpityadav112005@gmail.com>
2025-04-18 20:15:32 +0530

    edited the user.py

--- db/models/User.py
+++ db/models/User.py
@@ -4,11 +4,7 @@ class User:
     def __init__(self, username: str, password: str, email: str):
         self.username = username
         self.email = email
-        self.password = self.hash_password(password)
-
-    def hash_password(self, password: str):
-        """Hashes the password before storing it."""
-        return hashlib.sha256(password.encode('utf-8')).hexdigest()
+        self.password = password
 
     def to_dict(self):
         return {
@@ -17,4 +13,4 @@ class User:
             "password": self.password,
         }
     
-    
+
