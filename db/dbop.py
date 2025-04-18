1e2ba6db5e222adf3f39af69a09204eac3203c59
Arpityadav11 <arpityadav112005@gmail.com>
2025-04-18 19:11:51 +0530

    edited the relation

--- db/dbop.py
+++ db/dbop.py
@@ -10,7 +10,7 @@ def create_user(user: User):
     return user_collection.insert_one(user.dict())
 
 def read_user(user_id: str):
-    return user_collection.find_one({"_id": user_id})
+    return user_collection.find_one({"user_id": user_id})
 
 def update_user(user_id: str, user_data: dict):
     return user_collection.update_one({"_id": user_id}, {"$set": user_data})
@@ -22,24 +22,24 @@ def delete_user(user_id: str):
 def create_medical_record(medical: Medical):
     return medical_collection.insert_one(medical.dict())
 
-def read_medical_record(record_id: str):
-    return medical_collection.find_one({"_id": record_id})
+def read_medical_record(user_id: str):
+    return medical_collection.find_one({"_id": user_id})
 
-def update_medical_record(record_id: str, medical_data: dict):
-    return medical_collection.update_one({"_id": record_id}, {"$set": medical_data})
+def update_medical_record(user_id: str, medical_data: dict):
+    return medical_collection.update_one({"_id": user_id}, {"$set": medical_data})
 
-def delete_medical_record(record_id: str):
-    return medical_collection.delete_one({"_id": record_id})
+def delete_medical_record(user_id: str):
+    return medical_collection.delete_one({"_id": user_id})
 
 # CRUD operations for Achievement model
 def create_achievement(achievement: Achievement):
     return achievement_collection.insert_one(achievement.dict())
 
-def read_achievement(achievement_id: str):
-    return achievement_collection.find_one({"_id": achievement_id})
+def read_achievement(user_id: str):
+    return achievement_collection.find_one({"_id": user_id})
 
-def update_achievement(achievement_id: str, achievement_data: dict):
-    return achievement_collection.update_one({"_id": achievement_id}, {"$set": achievement_data})
+def update_achievement(user_id: str, achievement_data: dict):
+    return achievement_collection.update_one({"_id": user_id}, {"$set": achievement_data})
 
-def delete_achievement(achievement_id: str):
-    return achievement_collection.delete_one({"_id": achievement_id})
+def delete_achievement(user_id: str):
+    return achievement_collection.delete_one({"_id": user_id})
