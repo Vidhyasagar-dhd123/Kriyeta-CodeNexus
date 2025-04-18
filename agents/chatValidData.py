1df8a2f8212d0ec88d7f999c6b613fae20f1baa4
jaykum07 <kumrawatjay07@gmail.com>
2025-04-18 18:52:08 +0530

    Ai Models

--- agents/chatValidData.py
+++ agents/chatValidData.py
@@ -9,15 +9,11 @@ config = [{
 def get_data(boot):
     llm_config = {"config_list":config}
     prompt = [
-       "you are a planner your work is to check the data is related to medical or health issue or greeting message if is it then generate the output 'Yes' and if not then generate 'No' ",
-       "example data 'How are you??' -> 'Yes' 'i am nervous' -> 'No",
-       "give the response according to given data",
-       "example data 'How are you??' -> 'Yes' 'i am nervous' -> 'No",
-       "genrate the response if 'No' "
+       "You are a message checker if message related to medical or greeting then Yes othervise No",
+       "If response is yes then the user is asking valid question so answer the user if response is no then make user ask another question.",
+       "You are a message updator update the message and return user friendly response in only one single line."
     ]
     work1 = WorkFlow(prompt,llm_config,1)
     data = work1.get_output(boot)
     print(data)
     return data
-
-get_data("Hey how are you?")
